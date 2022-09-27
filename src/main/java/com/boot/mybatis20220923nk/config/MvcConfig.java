6092e3f33361a610b25b995e97bf4ddd6f1420f3
nage24 <n0rkjdx0@naver.com>
2022-09-28 00:45:33 +0900

    2022-09-27 MvcConfig 수정

--- src/main/java/com/boot/mybatis20220923nk/config/MvcConfig.java
+++ src/main/java/com/boot/mybatis20220923nk/config/MvcConfig.java
@@ -24,22 +24,16 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
-        registry.addResourceHandler() // Builder 형식
+        registry.addResourceHandler("/image/**") // Builder 형식
                 .addResourceLocations("file:///" + filePath) //
                 .setCachePeriod(60 * 60) // 60 초 * 60 ; 1 hour 동안 캐시 유지
                 .resourceChain(true)
-                .addResolver(new PathResourceResolver(){
+                .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         resourcePath = URLDecoder.decode(resourcePath, StandardCharsets.UTF_8);
                         return super.getResource(resourcePath, location);
                     }
                 });
-
-
     }
-
-
-
-
 }
